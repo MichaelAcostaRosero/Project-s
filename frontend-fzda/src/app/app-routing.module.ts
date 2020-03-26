2ecaed62208f716fad09736a5152495945b1a8a1
Fernando Zhunio <fzhunio91@hotmail.com>
2020-03-26 11:50:16 -0500

    anadido bootstrap, fontawesome, componentes: navBar, home, login, register, perfil

--- frontend-fzda/src/app/app-routing.module.ts
+++ frontend-fzda/src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PerfilComponent } from './logIn/PerfilModulo/Perfil/perfil/perfil.component';
+import { HomeComponent } from './Free/home/home.component';
+import { LoginComponent } from './logOut/login/login.component';
+import { RegisterComponent } from './logOut/register/register.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", redirectTo:'Home', pathMatch:'full'},
+  {path:'Home',component:HomeComponent},
+  {path:'Perfil',component:PerfilComponent},
+  {path:'Login',component:LoginComponent},
+  {path:"register", component:RegisterComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend-fzda/src/app/app.module.ts
+++ frontend-fzda/src/app/app.module.ts
@@ -4,15 +4,24 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { NavBarComponent } from './Componentes/nav-bar/nav-bar.component';
+import { PerfilModule } from './logIn/PerfilModulo/perfil.module';
+import { HomeComponent } from './Free/home/home.component';
+import { LoginComponent } from './logOut/login/login.component';
+import { RegisterComponent } from './logOut/register/register.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    PerfilModule
   ],
   providers: [],
   bootstrap: [AppComponent]
