adcf4572c1155e4c73c6194c816f9df749a58192
Fernando Zhunio <fzhunio91@hotmail.com>
2020-03-26 16:11:00 -0500

    Implentado las rutas: home, login, register, perfil, a medias el registerComponent

--- api_FZDA/config/app.php
+++ api_FZDA/config/app.php
@@ -161,6 +161,8 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        \Fruitcake\Cors\CorsServiceProvider::class,
+        Laravel\Passport\PassportServiceProvider::class,
 
         /*
          * Package Service Providers...
